9c0dc2aade5824f4d7eb544e450da7b88d2c74e0
Kamil Kowalewski <szamil128@onet.pl>
2020-01-04 12:06:18 +0100

    Add overrides to model

--- RazorPagesIntro/RazorPagesIntro/Models/Customer.cs
+++ RazorPagesIntro/RazorPagesIntro/Models/Customer.cs
@@ -1,10 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorPagesIntro.Models
 {
     public class Customer
     {
-        /*----------------------- PROPERTIES REGION ----------------------*/
         #region Properties
 
         public int Id { get; set; }
@@ -13,9 +13,38 @@ namespace RazorPagesIntro.Models
         public string Name { get; set; }
 
         #endregion
-        /*------------------------ METHODS REGION ------------------------*/
-        #region Methods
 
-        #endregion
+        public Customer()
+        {
+        }
+
+        public Customer(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        protected bool Equals(Customer other)
+        {
+            return Id == other.Id && Name == other.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Customer) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}";
+        }
     }
 }
