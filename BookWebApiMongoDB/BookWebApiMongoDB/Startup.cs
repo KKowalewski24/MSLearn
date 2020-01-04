343b31169dd59906bc354b4fbc90909909c49a19
Kamil Kowalewski <szamil128@onet.pl>
2020-01-04 21:39:51 +0100

    Initial commit + Add model

---
+++ BookWebApiMongoDB/BookWebApiMongoDB/Startup.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BookWebApiMongoDB
+{
+    public class Startup
+    {
+        /*----------------------- PROPERTIES REGION ----------------------*/
+        public IConfiguration Configuration { get; }
+
+        /*------------------------ METHODS REGION ------------------------*/
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
